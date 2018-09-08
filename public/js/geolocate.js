b317a8047e089fa7648143f7b90f3b62518f5063
Olga <oserenchenko@gmail.com>
2018-09-08 13:23:00 -0600

    grabbing user geolocation coordinates and input city coordinates

---
+++ public/js/geolocate.js
@@ -0,0 +1,30 @@
+//GEOLOCATION
+initMap = function() {
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      window.pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      $("#useGeolocation").attr({
+        lat: pos.lat,
+        lng: pos.lng
+      });
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+};
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation ?
+    "Error: The Geolocation service failed." :
+    "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
