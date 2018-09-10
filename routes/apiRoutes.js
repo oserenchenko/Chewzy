90a911839f3d26eeb5bdd07286d01193a4298111
Olga <oserenchenko@gmail.com>
2018-09-09 21:17:02 -0600

    zomato results loggin based on all user input

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,34 @@
 var db = require("../models");
-var restaurantData = [{
-  // one: "hello"
-}];
+var restaurantData = [
+  {
+    // one: "hello"
+  }
+];
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.get("/api/examples", function(req, res) {
     db.User.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
   // Get all examples
-  app.get("/api/search_results", function (req, res) {
+  app.get("/api/search_results", function(req, res) {
     res.json(restaurantData);
   });
 
   // Create a new example
-  app.post("/api/search_results", function (req, res) {
+  app.post("/api/search_results", function(req, res) {
     restaurantData.push(req.body);
     res.json(req.body);
-
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
+  app.delete("/api/examples/:id", function(req, res) {
     db.Users.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
