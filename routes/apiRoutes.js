7d692993a17cd9eaa8e81dd5214d4de1059edbe8
Inna <designbyinna@gmail.com>
2018-09-19 10:01:40 -0600

    removed extra files, added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,34 +2,6 @@ var db = require("../models");
 
 
 module.exports = function(app) {
-  app.get("/api/examples", function(req, res) {
-    db.User.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  app.post("/api/users", function(req, res) {
-    db.User.findOrCreate({
-      where: {
-        email: req.body.email
-      }
-    }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  app.get("/api/users", function(req, res) {
-    db.User.findAll({}).then(function(dbExample) {
-      var emailArr = [];
-      for (var i = 0; i < dbExample.length; i++) {
-        emailArr.push({
-          email: dbExample[i].email
-        });
-      }
-      res.json(emailArr);
-    });
-  });
-
   //======================
   //FAVORITES
   //======================
@@ -39,6 +11,4 @@ module.exports = function(app) {
       res.json(dbFavorite);
     });
   });
-
-  
 };
